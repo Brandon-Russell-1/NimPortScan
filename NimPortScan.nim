0fb83c1900c1f49f5228c320e16b6af4cd6be804
Brandon Russell <brandon.r.russell.1@gmail.com>
2024-05-19 11:07:40 -0500

    added error handling

--- NimPortScan.nim
+++ NimPortScan.nim
@@ -13,45 +13,48 @@ proc setSocketTimeout(socket: Socket, timeout: int) =
 
 # Function to scan a single port with a timeout
 proc scanPort(host: string, port: int, timeout: int): bool =
-  var socket = newSocket()
-  setSocketTimeout(socket, timeout)
+  var socket: Socket
   try:
+    socket = newSocket()
+    setSocketTimeout(socket, timeout)
     socket.connect(host, Port(port))
-    socket.close()
     return true
-  except OSError:
+  except OSError as e:
     return false
+  finally:
+    socket.close()
 
 # Function to scan a range of ports
-proc scanPorts(host: string, startPort, endPort, timeout: int) =
+proc scanPorts(host: string, startPort, endPort, timeout: int, showClosedPorts: bool) =
   echo &"Scanning {host} from port {startPort} to port {endPort}"
   for port in startPort..endPort:
     if scanPort(host, port, timeout):
       echo &"Port {port} is open"
-    else:
-      # echo &"Port {port} is closed"
+    elif showClosedPorts:
+      echo &"Port {port} is closed"
 
 # Function to parse command-line arguments
-proc parseArgs(): (string, int, int, int) =
-  if paramCount() != 4:
-    echo "Usage: port_scanner <IP address> <start port> <end port> <timeout>"
+proc parseArgs(): (string, int, int, int, bool) =
+  if paramCount() < 4 or paramCount() > 5:
+    echo "Usage: port_scanner <IP address> <start port> <end port> <timeout> [showClosedPorts]"
     quit(1)
   
   let ip = paramStr(1)
   let startPort = parseInt(paramStr(2))
   let endPort = parseInt(paramStr(3))
   let timeout = parseInt(paramStr(4))
+  let showClosedPorts = if paramCount() == 5: parseBool(paramStr(5)) else: false
   
   if not (0 < startPort and startPort <= 65535 and 0 < endPort and endPort <= 65535 and startPort <= endPort and timeout > 0):
     echo "Invalid input. Ports must be between 1 and 65535, start port must be less than or equal to end port, and timeout must be a positive integer."
     quit(1)
   
-  return (ip, startPort, endPort, timeout)
+  return (ip, startPort, endPort, timeout, showClosedPorts)
 
 # Main function
 proc main() =
-  let (host, startPort, endPort, timeout) = parseArgs()
-  scanPorts(host, startPort, endPort, timeout)
+  let (host, startPort, endPort, timeout, showClosedPorts) = parseArgs()
+  scanPorts(host, startPort, endPort, timeout, showClosedPorts)
 
 # Run the main function
 when isMainModule:
